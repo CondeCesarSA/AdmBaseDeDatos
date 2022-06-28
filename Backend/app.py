c373ef57e1ecefde18417b60f0cb506a237b78ad
Conde Cesar Santiago Andres <cesarsantiagoconde@gmail.com>
2022-06-28 16:33:01 -0300

    agregando backend a Frond end

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from routes.contacts import contacts
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+SQLAlchemy(app)
+
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@server/contactsdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.register_blueprint(contacts)
--- Backend/routes/contacts.py
+++ Backend/routes/contacts.py
@@ -9,3 +9,15 @@ def home():
 @contacts.route('/new')
 def add_contact():
     return "saving a contact"
+
+@contacts.route("about")
+def about():
+    return render_template('about.html')
+
+@contacts.route('/update')
+def update():
+    return "actualizando a contact"
+
+@contacts.route('/delete')
+def delete():
+    return "delete a contact"
