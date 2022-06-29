b9d5e86bdbbfdeb2abfeafcc4fd8193cf70dd553
Conde Cesar Santiago Andres <cesarsantiagoconde@gmail.com>
2022-06-29 03:18:30 -0300

    terminando la base CRUD del BACKEND

--- AplicacionWeb/BackEnd/backendintento2/sistema/libreria/models.py
+++ AplicacionWeb/BackEnd/backendintento2/sistema/libreria/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to='imagenes/',null = True, verbose_name='Imagen')
+    description = models.TextField(max_length=500, verbose_name='Descripcion')
+    
+    def __str__(self):
+        fila = "Titulo: " + self.titulo + " - " + "Descripcion: " + self.description
+        return fila
+    
+    def delete(self, user=None, keep_paterts=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+    
+    
+ 
--- AplicacionWeb/BackEnd/backendintento2/sistema/sistema/settings.py
+++ AplicacionWeb/BackEnd/backendintento2/sistema/sistema/settings.py
@@ -131,4 +131,10 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/imagenes/'
+
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
